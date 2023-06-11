1e0f8bc5b3c16c69307139ac73f274054a065ba9
jignesh-new <86604497+jignesh-new@users.noreply.github.com>
2023-06-11 19:49:45 +0530

    brnch changes

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <table class="table table-dark">
   <thead>
     <tr>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone No</th>
-      <th scope="col">Delete</th>
+      <th scope="col">First1</th>
+      <th scope="col">Last1</th>
+      <th scope="col">Email1</th>
+      <th scope="col">Phone No1</th>
+      <th scope="col">Delete1</th>
     </tr>
   </thead>
   <tbody id="fetchdata">
